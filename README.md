cf6b563d406d02054f1e5319f81b964e4e20d216
Azleur <ribonucleasa@gmail.com>
2025-04-16 10:40:01 -0400

    Add scripts/convert-json.py to process the new complete.json file; save a manually modified version of the output as data/hsk-manual.csv (WIP); document the source of complete.json

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Inspired by a similar project by Kostas.
 ## HSK Data
 
 XLSX file downloaded from [this Google Sheets](https://docs.google.com/spreadsheets/d/1SxoqHYYJOBF0TBHHkFJfwIR6RuQzfbr5c4wXn8cR54M/edit?usp=sharing), as posted in [this Reddit thread](https://www.reddit.com/r/datasets/comments/d9mxiy/i_have_compiled_a_dataset_of_11062_chinese/).
+
+`complete.json` file is MIT-licensed and available at [this GitHub repo](https://github.com/drkameleon/complete-hsk-vocabulary).
---
+++ scripts/convert-json.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+
+import json
+import pandas as pd
+from pretty_cli import PrettyCli
+
+
+def main() -> None:
+    cli = PrettyCli()
+    cli.main_title("Convert JSON")
+
+    # ================================================================ #
+    cli.section("Loading Data")
+
+    with open("data/complete.json", encoding="utf-8") as file:
+        js = json.load(file)
+
+    cli.print("Read OK")
+    # print(pd.Series(entry["level"] for entry in js).value_counts())
+    # return
+
+    # print(max(len(form["meanings"]) for entry in js for form in entry["forms"]))
+
+    # ================================================================ #
+    cli.section("Extracting Subset")
+
+    df = pd.DataFrame()
+
+    pinyin_counts = []
+
+    for entry in js:
+        levels = [l for l in entry["level"] if l.startswith("old-")]
+        if len(levels) < 1:
+            continue
+        level = int(levels[0][4:])
+
+        pinyin_counts.append(len(pd.Series([ form["transcriptions"]["pinyin"] for form in entry["forms"]]).unique()))
+
+        for form in entry["forms"]:
+            row = pd.DataFrame({
+                "hanzi": entry["simplified"],
+                "level": level,
+                "frequency": int(entry["frequency"]),
+                "pinyin": form["transcriptions"]["pinyin"],
+                "meanings": form["meanings"],
+            })
+
+            df = pd.concat([ df, row ])
+
+        # print(entry)
+        # break
+
+    df = df.sort_values(by=["level", "frequency", "pinyin"], ascending=True)
+
+    cli.print(df)
+    cli.small_divisor()
+    cli.print(pd.Series(pinyin_counts).describe())
+
+    # # ================================================================ #
+    cli.section("Saving Data")
+    df.to_csv("data/hsk.csv", index=False)
+    cli.print("Write OK")
+
+if __name__ == "__main__":
+    main()
