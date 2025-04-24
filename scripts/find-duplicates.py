9efcaa2f065a81fc745bcff533bb2f1297ea08d1
Azleur <ribonucleasa@gmail.com>
2025-04-23 22:23:06 -0400

    Add scripts/find-duplicates to help with data/hsk-manual cleanup. Add related .gitignore rule and fix minor issues in .gitignore.

---
+++ scripts/find-duplicates.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+
+import pandas as pd
+from pretty_cli import PrettyCli
+
+
+def main() -> None:
+    cli = PrettyCli()
+    cli.main_title("Find Duplicates")
+
+    # ================================================================ #
+    cli.chapter("Loading Data")
+    data = pd.read_csv("data/hsk-manual.csv")
+    cli.print("OK")
+
+    # ================================================================ #
+    cli.chapter("Stats")
+
+    # -------------------------------- #
+    cli.section("Total Entries per Level")
+    cli.print(data.groupby("level")["hanzi"].count())
+
+    # -------------------------------- #
+    cli.section("Unique Entries per Level")
+    cli.print(data.groupby("level")["hanzi"].nunique())
+
+    # -------------------------------- #
+    cli.section("Repeated Entries")
+
+    counts = data.groupby("level")["hanzi"].value_counts()
+    counts = counts[counts > 1]
+
+    cli.print(counts)
+    counts.to_csv("data/hsk-manual-repetitions.csv")
+
+
+if __name__ == "__main__":
+    main()
